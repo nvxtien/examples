4bb7cb1b9f894403723ffdc3d4074e13d22956a4
tien <nvxtien@gmail.com>
2019-01-07 17:56:39 +0700

    frobenius

--- src/main/java/com/tiennv/ec/Constants.java
+++ src/main/java/com/tiennv/ec/Constants.java
@@ -11,4 +11,15 @@ public class Constants {
     public static final BigInteger SIX = BigInteger.valueOf(6);
     public static final BigInteger TWO_FOUR = BigInteger.valueOf(24);
     public static final BigInteger THREE_SIX = BigInteger.valueOf(36);
+
+    public static final GFp2 XI = new GFp2(new GFp(BigInteger.ONE), new GFp(BigInteger.valueOf(3)));
+
+    public static final GFp2 XI_2PMinus2_Over3 = new GFp2(new GFp(BigInteger.ONE), new GFp(BigInteger.valueOf(3)));
+    public static final GFp2 XI_PMinus1_Over3 = new GFp2(new GFp(BigInteger.ONE), new GFp(BigInteger.valueOf(3)));
+    public static final GFp2 XI_PMinus1_Over6 = new GFp2(new GFp(BigInteger.ONE), new GFp(BigInteger.valueOf(3)));
+
+    //xi To 2PSquared Minus2 Over3
+    public static final GFp2 XI_2PSquaredMinus2_Over3 = new GFp2(new GFp(BigInteger.ONE), new GFp(BigInteger.valueOf(3)));
+    public static final GFp2 XI_2PSquaredMinus1_Over3 = new GFp2(new GFp(BigInteger.ONE), new GFp(BigInteger.valueOf(3)));
+
 }
